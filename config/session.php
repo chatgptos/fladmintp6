0e97c99c8bba6ea61979dce7eb6b7ca77b97b2fa
1276789849@qq.com <1276789849@qq.com>
2022-06-28 18:21:32 +0800

    compoer

--- config/session.php
+++ config/session.php
@@ -8,7 +8,7 @@ return [
     // session name
     'name'           => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
-    'var_session_id' => '',
+    'var_session_id' => 'fL_live_supply',
     // 驱动方式 支持file cache
     'type'           => 'file',
     // 存储连接标识 当type使用cache的时候有效
